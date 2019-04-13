054e5cd25182ed778fdc9c9d0b89cc205abe52da
ajunajunajun <ajunnajunn@gmail.com>
2019-04-14 04:09:54 +0900

    create GLView

--- App.js
+++ App.js
@@ -1,40 +1,44 @@
 import Expo, { GLView } from 'expo';
 import React from 'react';
 import { View } from 'react-native';
-import * as THREE from 'three';
+import * as THREE from "three";
 import ExpoTHREE from 'expo-three';
+
+console.disableYellowBox = true;
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <GLView
-          style={{ width:300, height:300 }}
-          onContextCreate={this._onGLContextCreate}
-        />
-      </View>
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate}
+      />
     )
   }
-  _onGLContextCreate = async (gl) => {
-    const width = gl.drawingBufferWidth;
-    const height = gl.drawingBufferHeight;
 
+  _onGLContextCreate = async (gl) => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
+      75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000
     );
-    const renderer = new ExpoTHREE.Renderer({ gl });
-    renderer.setSize(width, height);
+    const renderer = ExpoTHREE.createRenderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-    const geometry = new THREE.TetrahedronBufferGeometry(0.1, 0)
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-    const sphere = new THREE.Mesh( geometry, material );
-    scene.add(sphere);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
 
-    camera.position.z = 2;
+    camera.position.z = 5;
 
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.07;
+      cube.rotation.y += 0.04;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    }
+    animate();
   };
+
 }
